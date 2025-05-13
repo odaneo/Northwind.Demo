82701960c6084795434160dc85bfd6f9a9f10abe
oda <odaneo@outlook.com>
2025-05-13 20:30:06 +0900

    双指针

--- Northwind.Demo.Server.Test/Test1.cs
+++ Northwind.Demo.Server.Test/Test1.cs
@@ -6,8 +6,24 @@
         [TestMethod]
         public void TestMethod1()
         {
-        　　
+            int[] nums = { 1, 1, 1, 2, 2, 3 };
 
+            int n = nums.Length;
+            if (n <= 2)
+            {
+                Console.WriteLine(n); // 原数组已满足条件
+                return;
+            }
+            int left = 2;//左边
+
+            for (int right = 2; right < n; right++)
+            {
+                if (nums[right] != nums[left - 2])
+                {
+                    nums[left] = nums[right];
+                    left++;
+                }
+            }
         }
     }
 }
